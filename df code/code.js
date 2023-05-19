83d72ac992de8427981790287ac3a532dfe6f638
AMAYADIEGO <132412290+AMAYADIEGO@users.noreply.github.com>
2023-05-18 21:49:29 -0400

    Add files via upload

--- df code/code.js
+++ df code/code.js
@@ -91,3 +91,61 @@ function  existeDig3enN(){
     }
     N = parseInt(N / 10)
 }
+function calcularTrapecio(){
+    var h = parseInt(prompt("ingrese h ",10))
+    var b = parseInt(prompt("ingrese h",5))
+    var B = parseInt(prompt("ingrese h ",15))
+    var a = parseInt(prompt("ingrese h",10))
+    var c = parseInt(prompt("ingrese h ",7))
+    var area = 0
+    var perimetro = 0
+    area = ((b + B ) / 2) + h
+    perimetro = a + b + B + c
+    alert ("El perimetro es :" + area )
+    alert ("El perimetro es:" + perimetro) 
+}
+function DeterminarsiNesparoimpar(){
+    var N = parseInt(prompt("Ingrese N"))
+    if ((N % 2) == 0){
+        alert(N + "Es par")
+    } else {
+        alert(N + "Es impar")
+    }
+}
+
+function ifNormal(){
+    var N = parseInt(prompt("Ingrese N"))
+    if (N > 5 ) {
+        N = 10 
+    }else {
+        N = 1 
+    }
+    alert(N)
+}
+
+function ifAbreviado(){
+    var N = parseInt(prompt("Ingrese N"))
+    N = (N > 5) ? 10 : 1
+    alert(N)
+}
+/*function obtenerDigitoN(){
+    var N = parseInt(prompt("Ingrese A",10))
+    var cantidad = 0
+    while (N != 0){
+        N = parseInt(N/10)
+        cantidad = cantidad + 1
+        alert(N)
+    }
+    alert("digitos" + cantidad)
+}
+*/
+
+function obtenerCantidadDigN_FOR(){
+    var N = parseInt(prompt("Ingrese N",))
+    var cantidad = 0
+    for (cantidad  = 0 ; N != 0; cantidad++) {
+        N = parseInt(N / 10)
+    }
+    alert("digitos: " + cantidad)
+}
+    
